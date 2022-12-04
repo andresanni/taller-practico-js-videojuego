c2ce777f99e15573d9f2ed85309c352b93f234a5
Andres Anni <andres.anni1985@gmail.com>
2022-12-04 10:14:18 -0300

    Separamos la logica en 2 funciones, una que le de tamaño al canvas y otra que lo llene de elementos

--- main.js
+++ main.js
@@ -1,42 +1,43 @@
 const canvas= document.querySelector("#game");
 const context = canvas.getContext('2d');
+let canvasSize;
+let elementSize;
 
-window.addEventListener('load',startGame);
 
-function startGame(){
-    // context.fillRect(0,0,100,100); //Dibujar rectangulo con las coordenadas y el tamaño
-    // context.clearRect(0,0,50,50); //Borrar en forma de rectangulo con las coordenadas y el tamaño
-    
-    //Para darle estilo a los textos le asignamos valor a los siguientes variables:
-    // context.font= "25px Verdana";
-    // context.fillStyle = "purple";
-    // context.textAlign = "left"; //Alineacion con respecto al punto que marcan las coordenadas
-    // context.fillText("Andres",25,25);
+window.addEventListener('load',setCanvasSize);
+window.addEventListener('resize',setCanvasSize);
 
-    //Propiedades que nos devuelven el tamaño de nuestro HTML
-    // windows.innerHeight
-    // windows.innerWidth
-    let canvasSize;
-    if(window.innerHeight > window.innerWidth){ //Si el ancho del area visible del HTML es mayor que el alto
-        canvasSize = window.innerWidth * 0.8; //Tomamos el ancho como referencia y asignamos el 80 por ciento de su valor
-    } else{ //Si el largo es mayor que el ancho
-        canvasSize = window.innerHeight * 0.8; // El valor como referencia es el alto, tomamos el 80 por ciento del mismo
+
+
+function setCanvasSize(){
+
+    if(window.innerHeight > window.innerWidth){ 
+        canvasSize = window.innerWidth * 0.8; 
+    } else{ 
+        canvasSize = window.innerHeight * 0.8; 
     }
 
-    canvas.setAttribute('width',canvasSize); //Asigno el mismo valor al ancho y al alto para obtener un cuadrado
+    canvas.setAttribute('width',canvasSize); 
     canvas.setAttribute('height',canvasSize);
 
-    const elementSize = canvasSize / 10; //Variable dinamica con relacion al tamaño del canvas que ya habiamos calculado
-    console.log({canvasSize,elementSize});
+    elementSize = canvasSize / 10; 
 
-    context.font= elementSize + "px Verdana"; //reemplazamos el numero de pixeles por nuestra variable para que sea dinamico.
+
+    startGame();
+}
+
+
+
+
+
+function startGame(){
+
+    console.log({canvasSize,elementSize});
+    context.font= elementSize + "px Verdana"; 
     context.textAlign = "end";
-    for (let i = 1; i <= 10; i++) {
-        
-        context.fillText(emojis['I'],elementSize*i,elementSize);
-        
-    }
-    
+    for (let i = 1; i <= 10; i++) {        
+        context.fillText(emojis['I'],elementSize*i,elementSize);        
+    }    
 
 }
 
